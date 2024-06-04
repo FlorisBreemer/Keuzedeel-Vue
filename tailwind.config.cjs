9ef1723eb1365a0077d718ab1aa6de21a743a820
Floris Breemer <9018644@student.zadkine.nl>
2024-06-04 10:44:11 +0200

    Init Vue3 project with tailwindcss

---
+++ tailwind.config.cjs
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{html,js,ts,jsx,tsx,vue,css,scss,sass,less,styl,stylus,md,mdx,json,svg}'],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+}
